dfdb8dee1087a5c0734ff673e0b7009110f0b76b
Helder Ramos <hjbramos@gmail.com>
2018-03-10 12:52:39 -0400

    adicionado projeto do curso ifsp

---
+++ CursoIFSP/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	// Loop infinito
+	/*
+	int x =  0;
+	while(x < 10) {
+		printf("\nO valor de X eh %d", x);
+	}
+	*/
+	
+	/*
+	int cont = 0;
+	while (cont < 10) {
+		printf("%d ", cont);
+		cont = cont + 1;
+	}
+	*/
+	
+	/*
+	int n = 0;
+	int somatorio =  0;
+	while (n >= 0) {
+		printf("Digite um numero: ");
+		scanf("%d", &n);
+		
+		if(n>0) {
+			somatorio = somatorio + n;
+		}
+	}
+	printf ("O somatorio eh: %d", somatorio);
+	*/
+	
+	// Imprimindo os números pares até 50
+	/*
+	int i;
+	for (i=0; i <= 50; i=i+2) {
+		printf("%d", i);
+	}
+	*/
+	
+	
+	return 0;
+}
