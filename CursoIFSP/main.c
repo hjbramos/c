5fec0f6d25ead24aa76510b01577f4e332cfebf3
Helder Ramos <hjbramos@gmail.com>
2018-03-15 11:07:50 -0400

    codigo atualizado

--- CursoIFSP/main.c
+++ CursoIFSP/main.c
@@ -5,6 +5,75 @@
 
 int main(int argc, char *argv[]) {
 	
+	int A[5], i;
+	
+	A[0] = 2;
+	A[1] = 6;
+	A[2] = 7;
+	A[3] = 9;
+	A[4] = 11;
+	
+	for (i=0;i<5;i++) {
+		if(A[i] % 2 == 0) {
+			printf("%d \n", A[i]);
+		}
+	}
+	
+	/*
+	int i, n;
+	int vet[5];
+	
+	printf("Insira os valores no vetor: \n");
+	for (i=0; i<5; i++) {
+		scanf("%d", &vet[i]);
+	}
+	
+	printf("Impressao do Resultado: \n");
+	for(i=5-1; i>=0;i--)
+	{
+		printf("%d", vet[i]);
+	}
+	*/
+/*	
+	int soma = 0;
+	int v[10];
+	int i;
+	
+	for (i=0;i<10;i++)
+	{
+		printf("Entre com o elemento %i do vetor ", i + 1);
+		scanf("%d", &v[i]);
+	}
+	
+	for (i=0;i<10;i++) {
+		soma += v[i];
+	}
+	
+	printf("\nO valor da soma eh: %d", soma);
+	
+*/
+	
+	/*
+	float valorQualquer;
+	float v[10];
+	int i;
+	
+	for (i=0;i<10;i++)
+	{
+		printf("Entre com o elemento %i do vetor ", i + 1);
+		scanf("%f", &v[i]);
+	}
+	
+	valorQualquer = v[0];
+	for (i=0;i<10;i++) {
+		if(v[i] > valorQualquer) {
+			valorQualquer = v[i];
+		}
+	}
+	
+	printf("\nO valor encontrado acima eh:%.f", valorQualquer);
+	
+	*/
 	// Loop infinito
 	/*
 	int x =  0;
